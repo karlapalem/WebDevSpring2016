80e52f80b9ade0f3eab696178cbf05238e2567c9
karlapalem <karlapalem@ccs.neu.edu>
2016-04-02 00:24:00 -0400

    removed extra stuff

--- public/experiment/insta/server/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: process.env.db || 'localhost',
-  clientSecret: process.env.clientSecret || '79845e48d40e42cc9b4ad34509084c9a',
-  tokenSecret: process.env.tokenSecret || 'pick a hard to guess string'
-};
